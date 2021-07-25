c9fc2512e92c86ccd065d2ab8c3c390beddf5da5
schwendh <schwendh>
2021-07-25 10:43:26 +0200

    73. Order Controller Code Review

--- src/main/java/guru/sfg/brewery/repositories/security/perms/BreweryCreatePermission.java
+++ src/main/java/guru/sfg/brewery/repositories/security/perms/BreweryCreatePermission.java
@@ -1,14 +1,12 @@
 package guru.sfg.brewery.security.perms;
 
+
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/**
- * Created by jt on 6/30/20.
- */
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('beer.delete')")
-public @interface BeerDeletePermission {
+@PreAuthorize("hasAuthority('brewery.create')")
+public @interface BreweryCreatePermission {
 }
--- src/main/java/guru/sfg/brewery/repositories/security/perms/BreweryReadPermission.java
+++ src/main/java/guru/sfg/brewery/repositories/security/perms/BreweryReadPermission.java
@@ -5,10 +5,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/**
- * Created by jt on 6/30/20.
- */
 @Retention(RetentionPolicy.RUNTIME)
-@PreAuthorize("hasAuthority('beer.delete')")
-public @interface BeerDeletePermission {
+@PreAuthorize("hasAuthority('brewery.read')")
+public @interface BreweryReadPermission {
 }
