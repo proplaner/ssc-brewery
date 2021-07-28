a613771b175da1cb540a6949db654016eb3ab5b3
schwendh <schwendh>
2021-07-28 10:39:26 +0200

    126. Spring Security CORS Configuration

--- src/main/java/guru/sfg/brewery/config/WebConfig.java
+++ src/main/java/guru/sfg/brewery/config/WebConfig.java
@@ -1,17 +1,16 @@
 package guru.sfg.brewery.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * Created by jt on 7/28/20.
  */
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        //registry.
+//        registry.addMapping("/**").allowedMethods("GET", "POST", "PUT");
+//    }
 }
